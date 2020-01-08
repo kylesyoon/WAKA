3420c5ae9607def03ed2e968dfddc987343f14c0
Kyle Sejin Yoon <kylesyoon@fb.com>
2020-01-07 18:05:40 -0800

    Initial Commit

---
+++ WAKA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WAKA
+//
+//  Created by Kyle Sejin Yoon on 1/7/20.
+//  Copyright © 2020 Kyle Sejin Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
